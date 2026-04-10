d63b015c92de67fb7bda3814f5b34b34aa00ffe2
UmairAhmedImran <umairahmedimranbutt@gmail.com>
2026-04-10 22:39:46 +0500

    chore(feat): initial setup and adding shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,52 @@
-import { Button } from "@/components/ui/button"
+import Button from "../src/assets/Button.svg"
+import Button1 from "../src/assets/Button-1.svg"
+import Background from "../src/assets/Background.svg"
+import Background1 from "../src/assets/Background-1.svg"
+import Background2 from "../src/assets/Background-2.svg"
+import Container from "../src/assets/Container.svg"
+import Container1 from "../src/assets/Container-1.svg"
+import Container2 from "../src/assets/Container-2.svg"
+import Container3 from "../src/assets/Container-3.svg"
+import Container4 from "../src/assets/Container-4.svg"
+import Container5 from "../src/assets/Container-5.svg"
+import Container6 from "../src/assets/Container-6.svg"
+import Container7 from "../src/assets/Container-7.svg"
+import Container8 from "../src/assets/Container-8.svg"
+import Container9 from "../src/assets/Container-9.svg"
+
+import Text from "../src/assets/Text.png"
+import User from "../src/assets/User.png"
+import User1 from "../src/assets/User-1.png"
+import User2 from "../src/assets/User-2.png"
+import User3 from "../src/assets/User-3.png"
+
+
 
 export function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <>
+      <div>Hello World</div>
+      <div><img src={Button} /></div>
+      <div><img src={Button1} /></div>
+      <div><img src={Background} /></div>
+      <div><img src={Background1} /></div>
+      <div><img src={Background2} /></div>
+      <div><img src={Container} /></div>
+      <div><img src={Container1} /></div>
+      <div><img src={Container2} /></div>
+      <div><img src={Container3} /></div>
+      <div><img src={Container4} /></div>
+      <div><img src={Container5} /></div>
+      <div><img src={Container6} /></div>
+      <div><img src={Container7} /></div>
+      <div><img src={Container8} /></div>
+      <div><img src={Container9} /></div>
+      <div><img src={Text} /></div>
+      <div><img src={User} /></div>
+      <div><img src={User1} /></div>
+      <div><img src={User2} /></div>
+      <div><img src={User3} /></div>
+    </>
   )
 }
 
